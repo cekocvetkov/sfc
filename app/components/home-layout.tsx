0bc6fccbef2a8f7aeb1d1f897f309fae56cb500d
cekocvetkov <c.y.cvetkov@gmail.com>
2024-10-16 20:00:08 +0200

    fixed too long code width bug

--- app/components/home-layout.tsx
+++ app/components/home-layout.tsx
@@ -8,7 +8,7 @@ function HomeLayout({ children }: { children: React.ReactNode }) {
     <>
       <div className="h-[100%] flex flex-col items-start justify-start md:mt-[-200px] md:h-auto md:flex-co md:fixed md:inset-0  md:items-center md:justify-center">
         <div className="h-[100%] flex flex-col w-full md:flex-row md:w-[1056px] md:h-[620px] md:border-[0.1px] md:border-border-color">
-          <section className="flex flex-col w-full basis-[100%] h-[70%] px-5 md:basis-[75%] md:h-auto md:pl-32 md:pr-32">
+          <section className="flex flex-col w-full basis-[100%] h-[70%] px-5 md:basis-[75%] md:max-w-[75%] md:h-auto md:pl-32 md:pr-32">
             <Nav></Nav>
             {children}
           </section>
