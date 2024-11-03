9df08695c6b170ae277f3abb42f9d86e6403bdba
Tsvetan Tsvetkov <c.y.cvetkov@gmail.com>
2024-11-03 16:01:03 +0100

    fix build

--- app/hooks/UseScrollDirection.ts
+++ app/hooks/UseScrollDirection.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import useMobile from "./IsMobile";
 import { usePathname } from "next/navigation";
 const useScrollDirection = () => {
