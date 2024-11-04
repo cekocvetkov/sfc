55692ec5b02e18c785d9dfd032aec842c86ca6b4
Tsvetan Tsvetkov <c.y.cvetkov@gmail.com>
2024-11-04 19:14:22 +0100

    adjust colors

--- app/components/articles.tsx
+++ app/components/articles.tsx
@@ -34,7 +34,7 @@ function Articles({ initialArticles }: ArticlesProps) {
   getRandomColor();
 
   return (
-    <div className="flex w-full scrollbar-hide overflow-x-auto h-[151px] md:h-full md:flex-col md:max-w-[180px] md:border-x-[0.1px] md:border-border-color md:overflow-y-auto ">
+    <div className="bg-[#895f41] flex w-full scrollbar-hide overflow-x-auto h-[151px] md:h-full md:flex-col md:max-w-[180px] md:border-x-[0.1px] md:border-border-color md:overflow-y-auto ">
       {filteredArticles
         .filter((article: ArticleModel) => {
           // First check if there's a search string match
