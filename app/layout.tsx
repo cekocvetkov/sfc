1f5e0cae8fd8c52d23ba17d7637d39b30802a3d5
Tsvetan Tsvetkov <c.y.cvetkov@gmail.com>
2025-01-04 09:20:27 +0100

    Add goat counter analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,8 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <script data-goatcounter="https://sfcz.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
       <body className={`${inter.className} antialiased m-0 p-0 h-screen w-screen overflow-hidden bg-background-color text-primary-text-color`}>
         <div className="h-[100%] flex flex-col items-start justify-start md:mt-[-200px] md:h-auto md:flex-co md:fixed md:inset-0  md:items-center md:justify-center">
           <div className="h-[100%] flex flex-col w-full md:flex-row md:w-[1056px] md:h-[620px] md:border-[0.1px] md:border-border-color">
