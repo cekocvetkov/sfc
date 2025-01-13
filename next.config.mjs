3223e426b9bd1be38b8270b9c2605285de721c23
Tsvetan Tsvetkov <c.y.cvetkov@gmail.com>
2025-01-13 11:18:16 +0100

    Added umami redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "standalone" };
+const nextConfig = {
+  output: "standalone"
+  async rewrites() {
+    return [
+      {
+        source: "/umami.js",
+        destination: "http://192.168.0.10:3000/script.js"
+      },
+      {
+        source: "/api/send",
+        destination: "http://192.168.0.10:3000/api/send"
+      }
+    ]
+  }
+};
 
 export default nextConfig;
